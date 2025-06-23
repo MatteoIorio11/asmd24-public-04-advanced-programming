53b0c81e885149c6fba4652bbc3cf45898693d5b
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-23 15:38:43 +0200

    doc: add axioms rules for ArrayList

--- src/test/scala/u04/array/ArrayListSpec.scala
+++ src/test/scala/u04/array/ArrayListSpec.scala
@@ -8,8 +8,13 @@ import scala.jdk.CollectionConverters._
 /**
  * Extend to testing Java standard library classes like java.util.ArrayList or java.util.TreeSet
  */
-object ArrayListSpec extends Properties("ArrayList") {
-
+object ArrayListSpec extends Properties("ArrayList"):
+  /** Preserve Insertion Order
+   * Every time a new element is pushed inside the ArrayList, the order is preserved.
+   * add(1) = ArrayList(1)
+   * add(2) = ArrayList(1, 2)
+   * add(-1) = ArrayList(1, 2, -1)
+   */
   property("preserves insertion order") = Prop.forAll(Gen.listOf(Gen.alphaStr)) { xs =>
     val list = new ArrayList[String]()
     xs.foreach(list.add)
@@ -17,6 +22,13 @@ object ArrayListSpec extends Properties("ArrayList") {
     xs == list.asScala.toList
   }
 
+  /** Data Store Law
+   * Every time an element is pushed inside the ArrayList it is preserved, no matter if it is already stored.
+   * add(1) = ArrayList(1)
+   * add(2) = ArrayList(1, 2)
+   * add(-1) = ArrayList(1, 2, -1)
+   * add(1) = ArrayList(1, 2, -1, 1)
+   */
   property("size matches number of inserted elements") = Prop.forAll(Gen.listOf(Gen.alphaStr)) { xs =>
     val list = new ArrayList[String]()
     xs.foreach(list.add)
@@ -24,6 +36,13 @@ object ArrayListSpec extends Properties("ArrayList") {
     list.size() == xs.size
   }
 
+  /** Contains Law
+   * Every time we check if the ArrayList contains an existing value, it must return true
+   * add(1) = ArrayList(1)
+   * add(2) = ArrayList(1, 2)
+   * add(-1) = ArrayList(1, 2, -1)
+   * -1 in ArrayList ? == True
+   */
   property("contains returns true for added elements") = Prop.forAll(Gen.listOf(Gen.alphaStr)) { xs =>
     val list = new ArrayList[String]()
     xs.foreach(list.add)
@@ -31,6 +50,13 @@ object ArrayListSpec extends Properties("ArrayList") {
     xs.forall(list.contains)
   }
 
+  /** Shrinking Law
+   * Every time we remove an element that is stored inside the ArrayList, it shrinks its size by one.
+   * add(1) = ArrayList(1)
+   * add(2) = ArrayList(1, 2)
+   * add(-1) = ArrayList(1, 2, -1)
+   * remove(1, ArrayList) = ArrayList(2, -1)
+   */
   property("removal reduces size by one") = Prop.forAll(Gen.nonEmptyListOf(Gen.alphaStr)) { xs =>
     val list = new ArrayList[String]()
     xs.foreach(list.add)
@@ -40,5 +66,4 @@ object ArrayListSpec extends Properties("ArrayList") {
 
     removed && list.size() == sizeBefore - 1
   }
-}
 
