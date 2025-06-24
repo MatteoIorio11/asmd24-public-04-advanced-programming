2573de73aff05d3a055e27eeb66deec36eb72894
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-24 12:16:07 +0200

    doc: add axiom description

--- src/test/scala/u04/point/PointCheck.scala
+++ src/test/scala/u04/point/PointCheck.scala
@@ -9,7 +9,7 @@ import scala.math.Pi
  * Write a comprehensive ScalaCheck test suite that verifies mathematical properties such as: Distance symmetry (i.e., a.distanceTo(b) == b.distanceTo(a)), Triangle inequality, Rotation
  * invariants
  */
-object Point2DGenerators {
+object Point2DGenerators:
   val genDouble: Gen[Double] = Gen.choose(-1e6, 1e6)
 
   val genPoint: Gen[(Double, Double)] = for {
@@ -21,16 +21,19 @@ object Point2DGenerators {
     genPoint.map { case (x, y) => new Point2D(x, y) }
 
   val genAngle: Gen[Double] = Gen.choose(-Pi, Pi)
-}
 
 /**
  * Write a comprehensive ScalaCheck test suite that verifies mathematical properties such as: Distance symmetry (i.e., a.distanceTo(b) == b.distanceTo(a)), Triangle inequality, Rotation
  * invariants
  */
-object Point2DSpec extends Properties("Point2D") {
+object Point2DSpec extends Properties("Point2D"):
   import Point2DGenerators._
 
-
+  /** Distance Law
+   * The distance between two points should be the same.
+   * distance(a, b) == distance(b, a)
+   * distance(c, b) == distance(b, c)
+   */
   property("distance symmetry") = Prop.forAll(genPoint2D, genPoint2D) { (a, b) =>
     val d1 = a.distanceTo(b)
     val d2 = b.distanceTo(a)
@@ -49,6 +52,10 @@ object Point2DSpec extends Properties("Point2D") {
   }
 
 
+  /** Rotation Law
+   * If we rotate a point, the distance to the origin should be always the same.
+   * distance(origin, a) == distance(origin, rotate(a, angle))
+   */
   property("rotation preserves distance to origin") = Prop.forAll(genPoint2D, genAngle) { (p, angle) =>
     val origin = new Point2D(0, 0)
     val rotated = p.rotate(angle)
@@ -57,10 +64,13 @@ object Point2DSpec extends Properties("Point2D") {
     (d1 - d2).abs < 1e-9
   }
 
+  /** Full Rotation Law
+   * If we rotate the point of 2pi, the point should be on the same starting point.
+   * distance(a, rotate(a, 2pi)) ≃ 0.0
+   */
   property("rotate full circle yields same point (approximately)") = Prop.forAll(genPoint2D) { p =>
     val fullCircle = 2 * Pi
     val rotated = p.rotate(fullCircle)
     p.distanceTo(rotated) < 1e-9
   }
-}
 
