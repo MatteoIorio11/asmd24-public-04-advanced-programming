331927e69b3217ad455458e110adeeb4d1917317
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-29 16:01:45 +0200

    feat: add public to constructor

--- src/main/java/u04/point/Point2D.java
+++ src/main/java/u04/point/Point2D.java
@@ -1,13 +1,11 @@
-package scala.u04.point;
-
-import scala.u04.datastructures.Functions;
+package u04.point;
 
 /**
  * Create a simple immutable Java class (e.g., Point2D with x, y coordinates and methods like distanceTo, translate, rotate)
  */
 public final class Point2D {
     final private Double x, y;
-    Point2D(final Double x, final Double y) {
+    public Point2D(final Double x, final Double y) {
         this.x = x;
         this.y = y;
     }
